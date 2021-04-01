140f16354716c18888d74796332661105b3da1e0
Ndeep <kandpal5@gmail.com>
2021-04-01 16:56:37 +0530

    add new function

--- aws_demo/testapp/urls.py
+++ aws_demo/testapp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,type_my_name
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('print_me/<str:name>',type_my_name)
 ]
--- aws_demo/testapp/views.py
+++ aws_demo/testapp/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h2>You have reached the homepage</h2>")
+
+def type_my_name(request,name):
+    return HttpResponse("<h2>Hello,{0}</h2>".format(name))
